7c16d0e4568dda230c10c50a2cb45a93db1bdd0c
Kajal Patel <kpate223@uwo.ca>
2020-03-29 12:04:50 -0400

    forgot to save file

--- 215.kth-largest-element-in-an-array.java
+++ 215.kth-largest-element-in-an-array.java
@@ -1,3 +1,5 @@
+import java.util.PriorityQueue;
+
 /*
  * @lc app=leetcode id=215 lang=java
  *
@@ -7,8 +9,30 @@
 // @lc code=start
 class Solution {
     public int findKthLargest(int[] nums, int k) {
+        //create a maxHeap, storing up to nums.length-k elements - > because we want to keep the largest element at the top
+        
+        //the top of the minHeap will be the Kth largest element
+        //keep the k smallest elements
+        int maxHeapSize = nums.length-k+1; //because 0 is the 0th smallest element
+        
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b)->b-a);
+        
+        //add all the elements to the heap
+        for (int num: nums){
+            maxHeap.add(num);
+            
+            //if the max heap size is reached, remove the current largest element
+            if (maxHeap.size()>maxHeapSize){
+                maxHeap.remove();
+            }
+        }
+        
+        //return the top of the heap
+        return maxHeap.remove();
         
     }
+    }
+
 }
 // @lc code=end
 
